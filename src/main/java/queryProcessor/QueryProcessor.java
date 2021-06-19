dc74f31c11e66199d4b7e4be33b684e5d089ed53
Philemon Lee <philemon.lee@dal.ca>
2021-06-19 14:04:58 -0300

    Created QueryProcessor module

---
+++ src/main/java/queryProcessor/QueryProcessor.java
@@ -0,0 +1,23 @@
+package queryProcessor;
+
+public class QueryProcessor {
+    public String processQuery(String query) {
+        String queryType = null;
+        if (true) {
+            queryType = "SELECT";
+        }
+        if (true) {
+            queryType = "UPDATE";
+        }
+        if (true) {
+            queryType = "INSERT";
+        }
+        if (true) {
+            queryType = "TABLE CREATE";
+        }
+        if (true) {
+            queryType = "DATABASE CREATE";
+        }
+        return queryType;
+    }
+}
