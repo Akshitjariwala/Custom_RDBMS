cc2b66708f9845db759892692144f59718398337
Philemon Lee <philemon.lee@dal.ca>
2021-08-01 16:21:01 -0300

    Created test for delete

--- src/main/java/queryProcessor/Delete.java
+++ src/main/java/queryProcessor/Delete.java
@@ -1,13 +1,29 @@
 package queryProcessor;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Delete {
+    private static final String workingDir = System.getProperty("user.dir");
+
     public static void execute() {
+        String databaseName = "database1";
+        String tableName = "user_data";
         // Sample query: delete from table_b where column1=100 and column2=akshit and column3 =100
         // TODO: Get tokens
         // tableName
         // condition(s) - columns/values
         // TODO: Open table file
-        // TODO: Do a search for values
+        try (BufferedReader br = new BufferedReader(new FileReader(workingDir + "/appdata/database/" + databaseName + "/" + tableName + ".txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         // TODO: Delete values
         // TODO: Save edits
         // TODO: Close file
---
+++ src/test/java/queryProcessor/DeleteTest.java
@@ -0,0 +1,13 @@
+package queryProcessor;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeleteTest {
+
+    @Test
+    void execute() {
+        Delete.execute();
+    }
+}
