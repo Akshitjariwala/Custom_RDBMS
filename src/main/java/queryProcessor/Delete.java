c8695b74e16ce6cdfbc5f03f6844cb5e9ff93f1c
Philemon Lee <philemon.lee@dal.ca>
2021-08-01 15:54:18 -0300

    Added TODO to Delete.java

--- src/main/java/queryProcessor/Delete.java
+++ src/main/java/queryProcessor/Delete.java
@@ -2,6 +2,14 @@ package queryProcessor;
 
 public class Delete {
     public static void execute() {
-
+        // Sample query: delete from table_b where column1=100 and column2=akshit and column3 =100
+        // TODO: Get tokens
+        // tableName
+        // condition(s) - columns/values
+        // TODO: Open table file
+        // TODO: Do a search for values
+        // TODO: Delete values
+        // TODO: Save edits
+        // TODO: Close file
     }
 }
