13f35f4c18dd814bf32651b161d2e05a7a4792bf
Philemon Lee <philemon.lee@dal.ca>
2021-08-01 18:19:45 -0300

    Added matric to select

--- src/main/java/queryProcessor/Select.java
+++ src/main/java/queryProcessor/Select.java
@@ -3,10 +3,7 @@ package queryProcessor;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Select {
     private static final String workingDir = System.getProperty("user.dir");
@@ -19,12 +16,11 @@ public class Select {
         searchTerms.put("user_name", "alex");
         searchTerms.put("user_email", "alex.mark@gmail.com");
         searchTerms.put("user_contact", "5566223311");
-        // Sample query: delete from table_b where column1=100 and column2=akshit and column3 =100
+        // Sample query: select column1,column2,column3 from table_a where userid = 100 and user = akshit
         // TODO: Get tokens
         // tableName
         // condition(s) - columns/values
         // TODO: Open table file
-        int i = 0;
         List<String> rows = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(workingDir + "/appdata/database/" + databaseName + "/" + tableName + ".txt"))) {
             String line;
@@ -34,5 +30,36 @@ public class Select {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        // Get number of columns
+        int colSize = rows.get(0).split("\\|\\|").length;
+        int rowSize = rows.size();
+        String[][] tableMatrix = new String[colSize][rowSize];
+        for (int i = 0; i < rowSize; i++) {
+            System.out.println(rows.get(i));
+            String[] columnValues = rows.get(i).split(("\\|\\|"));
+            for (int j = 0; j < colSize; j++) {
+                tableMatrix[i][j] = columnValues[j].trim();
+            }
+        }
+        // Match select columns to index number
+        Map<String, Integer> indexOfSearchColumns = new HashMap<>();
+        for (String s : columnsName) {
+            for (int j = 0; j < rowSize; j++) {
+                if (tableMatrix[0][j].equals(s)) {
+                    System.out.println(tableMatrix[0][j] + " found at " + j);
+                    indexOfSearchColumns.put(tableMatrix[0][j], j);
+                }
+            }
+        }
+
+        // Print matrix
+        for (int i = 0; i < colSize; i++) {
+            for (int j = 0; j < rowSize; j++) {
+                String item = tableMatrix[i][j];
+                System.out.print(item + "\t");
+            }
+            System.out.print("\n");
+        }
+            // TODO: Output results
     }
 }
---
+++ src/test/java/queryProcessor/SelectTest.java
@@ -0,0 +1,13 @@
+package queryProcessor;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SelectTest {
+
+    @Test
+    void execute() {
+        Select.execute();
+    }
+}
