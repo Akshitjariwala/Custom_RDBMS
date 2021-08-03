e0d3ed1994444b3aa714cbe76c385d5f38b58e87
Akshit Jariwala <ak443157@dal.ca>
2021-08-03 15:57:48 +0530

    Some more refactoring in Query validation module and Insert query validation.

--- src/main/java/queryValidator/insertValidation.java
+++ src/main/java/queryValidator/insertValidation.java
@@ -15,10 +15,11 @@ public class insertValidation {
         int lengthOfValues = 7;
         String columnSubString = "";
         String valuesSubString = "";
-        int indexOfValues = query.toUpperCase().indexOf("VALUES");
+        int indexOfValues1 = query.toUpperCase().indexOf("VALUES");
+        int indexOfValues = query.toUpperCase().indexOf("(",indexOfValues1);
         int indexOfValuesEnd = query.toUpperCase().indexOf(")",indexOfValues);
         String[] tempArray = new String[queryTokens.length];
-        String insertPattern = "[I-i][N-n][S-s][E-e][R-r][T-t]\\s+[I-i][N-n][T-t][O-o]\\s+[A-Za-z0-9_]+\\([A-Za-z0-9,_]+\\)\\s*[V-v][A-a][L-l][U-u][E-e][S-s]\\s*\\([A-Za-z0-9,_.@\\s\"]+\\)";
+        String insertPattern = "[I-i][N-n][S-s][E-e][R-r][T-t]\\s+[I-i][N-n][T-t][O-o]\\s+[A-Za-z0-9_]+\\s*\\([A-Za-z0-9,_]+\\)\\s*[V-v][A-a][L-l][U-u][E-e][S-s]\\s*\\([A-Za-z0-9,_.@\\s\"]+\\)";
         int indexOfInto = query.toUpperCase().indexOf("INTO");
         Map<String,Object> tokens = new HashMap<>();
         String tableName = query.substring(indexOfInto+4,query.indexOf("(")).trim();
@@ -33,7 +34,7 @@ public class insertValidation {
             columnSubString = columnSubString.trim();
             String[] columnsArray = columnSubString.split("\\s*,\\s*");
             tokens.put("columnNames",tableValidationMethods.createListFromArray(columnsArray));
-            valuesSubString = query.substring(indexOfValues+lengthOfValues,indexOfValuesEnd);
+            valuesSubString = query.substring(indexOfValues+1,indexOfValuesEnd);
             valuesSubString = valuesSubString.trim();
             String[] valuesArray = valuesSubString.replaceAll("\"","").split("\\s*,\\s*");
             tokens.put("columnValues",tableValidationMethods.createListFromArray(valuesArray));
