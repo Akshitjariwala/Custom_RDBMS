58515e02165c7cf0fd2eb45a3a354a4813ff95a6
Rishita Kotiyal <rs677988@dal.ca>
2021-08-03 17:46:03 +0530

    Added ddTableDrop Method

--- src/main/java/queryProcessor/Drop.java
+++ src/main/java/queryProcessor/Drop.java
@@ -1,7 +1,11 @@
 package queryProcessor;
 
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
 import java.util.Map;
 
 public class Drop {
@@ -9,6 +13,19 @@ public class Drop {
     }
 
     public void ddTableDrop(String tableName) throws IOException {
-
+        String databasePath = "appdata/database/database1/";
+        String ddFile = "data_dictionary.txt";
+        //String tempFile = "temp";
+        //System.out.println("debug3");
+        File file  = new File(databasePath + ddFile);
+        File temp = new File(file.getAbsolutePath() + ".tmp");
+        PrintWriter out = new PrintWriter(new FileWriter(temp));
+        Files.lines(file.toPath())
+                .filter(line -> !line.contains(tableName))
+                .forEach(out::println);
+        out.flush();
+        out.close();
+        file.delete();
+        temp.renameTo(file);
     }
 }
